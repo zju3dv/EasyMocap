3f2aec6b44b07cb90b6242a16c76385b365cff9d
shuaiqing <s_q@zju.edu.cn>
2021-04-13 17:57:39 +0800

    :fire: remove unused files and images

--- scripts/calibration/calib_intri.py
+++
@@ -1,7 +0,0 @@
-'''
-  @ Date: 2021-03-02 16:12:59
-  @ Author: Qing Shuai
-  @ LastEditors: Qing Shuai
-  @ LastEditTime: 2021-03-02 16:12:59
-  @ FilePath: /EasyMocap/scripts/calibration/calib_intri.py
-'''
